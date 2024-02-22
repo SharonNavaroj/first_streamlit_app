c8bb3e223bbb98889f4e1d456337848146e9d41e
SharonNavaroj <160929445+SharonNavaroj@users.noreply.github.com>
2024-02-23 00:01:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.title('Breakfast Menu')
 streamlit.title('HArdboiled Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
